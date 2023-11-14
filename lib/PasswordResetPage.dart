704975c088fbcf143fb25b38ce5c8fd857802c4e
Pallavi <97425707+Pallavi-kemburu@users.noreply.github.com>
2023-11-14 10:17:40 +0530

    2.5.2

--- lib/PasswordResetPage.dart
+++ lib/PasswordResetPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:learnx/LoginPage.dart';
 
 class PasswordReset extends StatefulWidget{
@@ -52,54 +53,124 @@ class _PasswordResetState extends State<PasswordReset>{
   Widget build(BuildContext context){
     double wi = MediaQuery.of(context).size.width;
     double hi = MediaQuery.of(context).size.height;
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 230, 230, 230),
-      body: Center(
-            child: Form(
-              key: _formkey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  
-                  Text("Please enter your Registered mail"),
+    return SafeArea(
+        child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        // backgroundColor: Colors.orangeAccent,
+        body: Stack(
+          children: [
+             Container(
+        height: hi*0.30,
+        width: wi,
+        decoration: BoxDecoration(
+        image: DecorationImage(image: NetworkImage("https://cdni.iconscout.com/illustration/premium/thumb/forgot-password-mobile-8044866-6430775.png?f=webp"),
+        fit: BoxFit.contain, alignment: Alignment.topCenter,),
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(wi*0.25),bottomRight:Radius.circular(wi*0.25), ),
+        color: Colors.teal[400],
+        )
+       ),//Orange layyer
+
+    SizedBox(
+    height: hi,
+    width: wi,
+    child: Padding(
+    padding: EdgeInsets.symmetric(horizontal: wi*0.03),
+    child: Form(
+    key: _formkey,
+    child: Center(
+    child: Column(
+    children: [
+    SizedBox(height: hi*0.32,),
+    Text(
+    "Forgot your Password ?",
+    style: GoogleFonts.poppins(
+    textStyle: TextStyle(
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+    fontSize: wi*0.06,
+    )
+    ),
+    ),
+     // Text("Did you forgot your password?",style:GoogleFonts.poppins(textStyle: TextStyle(fontWeight: FontWeight.bold,fontSize:wi*0.045),),),
+      SizedBox(height: hi*0.025,),
+      Center(child: Text("That's ok...",style: GoogleFonts.poppins(textStyle: TextStyle(fontSize: wi*0.035),)),),
+      SizedBox(height: hi*0.01,),
+      Text("Just enter the email address you've used to",style: GoogleFonts.poppins(textStyle: TextStyle(fontSize: wi*0.035),)),
+      SizedBox(height: hi*0.01,),
+      Text("register with us and we'll send you a reset link!",style: GoogleFonts.poppins(textStyle: TextStyle(fontSize: wi*0.035),)),
+      SizedBox(height: hi*0.03,),
+
+      Padding(
+        padding:  EdgeInsets.symmetric(horizontal: wi*0.02),
+        child: TextFormField(
+    keyboardType: TextInputType.text,
+    controller: _email,
+    validator: (val) => val!.isEmpty ? "Please Enter Email" : null,
+    decoration: InputDecoration(
+
+    prefixIcon: Icon(
+    CupertinoIcons.mail,size: hi*0.02,
+    ),
+    hintText: "Enter Your Email",
+    hintStyle: GoogleFonts.poppins(
+    textStyle: TextStyle(
+
+    color: Colors.grey.shade600,
+    fontSize: hi*0.02,
+    ),
+    ),
+    ),
+
+    ),
+      ),//email
 
 
-                  TextFormField(
-                    controller: _email,
-                    focusNode: _focusNode,
-                    textInputAction: TextInputAction.done,
-                    onEditingComplete: () => _focusNode.unfocus(),
-                    decoration: const InputDecoration(
-                      suffixIcon: Icon(Icons.mail),
-                      hintText: "Enter Email",
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                            Radius.circular(10)),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                              Radius.circular(10)),
-                          borderSide: BorderSide(
-                              color: Colors.blue
-                          )
-                      ),
-                    ),
-                    validator: (val)=>val!.isEmpty?"Enter Valid Email":null,
-                  ),
-                  
-                  
-                  ElevatedButton(
-                      onPressed: (){
-                        if (_formkey.currentState!.validate()){
-                          resetPassword();
-                        }
-                      },
-                      child: const Text("Reset Password"),
-                  )
-                ],
-              )
-          )
-      ),
+    SizedBox(height: hi*0.07,),
+
+
+    InkWell(
+    onTap: (){
+    if (_formkey.currentState!.validate()){
+    resetPassword();
+    }
+    },
+    child: Container(
+    width: wi*0.65,
+    height: hi*0.05,
+    decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(hi*0.025),
+  //  border: Border.all(color: Colors.orangeAccent),
+    color: Color.fromARGB(255,240,88,69),
+
+    ),
+    child: Center(
+    child: Text(
+    "SUBMIT",
+    textAlign: TextAlign.center,
+    style: GoogleFonts.poppins(
+    textStyle: TextStyle(
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+    fontSize: hi*0.03,
+    )
+    ),
+    ),
+    ),
+    ),
+    ),//Sign in
+    SizedBox(height: hi*0.07,),
+
+    ],
+    ),
+    ),
+    )
+    ),
+    ),
+
+    ]
+
+    ),
+        )
     );
   }
 }
--- lib/settings_ui.dart
+++ lib/settings_ui.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:learnx/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 class settings_page extends StatefulWidget {
@@ -12,16 +14,24 @@ class settings_page extends StatefulWidget {
 }
 
 class _settings_pageState extends State<settings_page> {
+  DarkThemeProvider themeChangeProvider = new DarkThemeProvider();
+
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentAppTheme();
+  }
+
+  void getCurrentAppTheme() async {
+    themeChangeProvider.darkTheme =
+    await themeChangeProvider.darkThemePreference.getTheme();
+  }
 
   Future<void> logoutUser(BuildContext context) async {
-    await FirebaseAuth.instance.signOut().
-    then((value){
-      showCustomSnackBar('Logged out Successfully!',Color.fromARGB(255, 66, 146, 130));
-      Navigator.pushReplacementNamed(context, '/login');
-    }).
-    onError((error, stackTrace){
-      showCustomSnackBar('Logout Failed!',Color.fromARGB(255, 66, 146, 130));
-    });
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => MyHome()));
   }
   void showCustomSnackBar(String string, Color color) {
     ScaffoldMessenger.of(context).showSnackBar(
@@ -39,8 +49,10 @@ class _settings_pageState extends State<settings_page> {
     );
   }
 
+
   @override
   Widget build(BuildContext context){
+   // var darkThemeProvider = Provider.of<DarkThemeProvider>(context);
     double wi = MediaQuery.of(context).size.width;
     double hi = MediaQuery.of(context).size.height;
     return SafeArea(
@@ -122,30 +134,73 @@ class _settings_pageState extends State<settings_page> {
                 ),
               ),
               GestureDetector(
-                onTap: (){},
-                child: ListTile(
-                  leading: Icon(
-                    CupertinoIcons.arrow_right_to_line,
-                    color: Colors.black,
-                  ),
-                  title: Text(
-                    "Logout",
-                    textAlign: TextAlign.left,
-                    style: GoogleFonts.poppins(
-                        textStyle: TextStyle(
-                          color: Colors.black,
-                          fontSize: wi*0.045,
-                        )
+                  child: ListTile(
+                    leading: Icon(
+                      CupertinoIcons.arrow_right_to_line,
+                      color: Colors.black,
+                    ),
+                    title: Text(
+                      "Logout",
+                      textAlign: TextAlign.left,
+                      style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                            color: Colors.black,
+                            fontSize: wi*0.045,
+                          )
+                      ),
                     ),
                   ),
+                onTap: (){
+                  showDialog(context: context, builder: (context) {
+                    return AlertDialog(
+                      title: Text("Logout"),
+                      content: Text("Are you sure to logout?"),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      actions:[
+                        TextButton(
+                          child: Text("NO",style: GoogleFonts.poppins(textStyle: TextStyle()),
+                          ),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        TextButton(
+                            onPressed: (){
+                              logoutUser(context);
+                            },
+                            child: Text("YES",style: GoogleFonts.poppins(textStyle: TextStyle()
+                            ),
+                            )
+                        )
+                    ]
+                    );
+                  }
+                  );
+                }
+             ),
+              GestureDetector(
+                child: ListTile(
+                  leading: Icon(Icons.dark_mode),
+                  title: Text("Dark Mode"),
+                  trailing:CupertinoSwitch(
+    value: themeChangeProvider.darkTheme,
+    onChanged: (bool value) {
+    setState(() {
+      value=!value;
+    });
+    }
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ),
+              )
+            ]
+         )
+       )
       )
     );
   }
+
+
   AboutApp(){
     double wi = MediaQuery.of(context).size.width;
     double hi = MediaQuery.of(context).size.height;
@@ -213,5 +268,33 @@ class _settings_pageState extends State<settings_page> {
     );
   }
 }
+
+class DarkThemeProvider with ChangeNotifier {
+  DarkThemePreference darkThemePreference = DarkThemePreference();
+  bool _darkTheme = false;
+
+  bool get darkTheme => _darkTheme;
+
+  set darkTheme(bool value) {
+    _darkTheme = value;
+    darkThemePreference.setDarkTheme(value);
+    notifyListeners();
+  }
+}
+
+class DarkThemePreference {
+  static const THEME_STATUS = "THEMESTATUS";
+
+  setDarkTheme(bool value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool(THEME_STATUS, value);
+  }
+
+  Future<bool> getTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(THEME_STATUS) ?? false;
+  }
+}
+
 String info =
-"Welcome to LearnX, your one-stop destination for a diverse range of technology-related courses and insightful blogs. Whether you're a beginner or an experienced professional, our platform is designed to empower you with knowledge across various tech domains. Immerse yourself in a rich learning experience, explore captivating blogs, and challenge your mind with daily puzzles.\n\nKey Features:\n\n1. Extensive Course Library:\n   Dive into our extensive library of technology courses covering a wide array of topics. From programming languages to cutting-edge technologies, we've got you covered.\n\n2. Informative Blogs:\n   Enhance your understanding of technology through freely accessible blogs. Our platform offers a wealth of information, keeping you updated on the latest trends, best practices, and insights from industry experts.\n\n3. Daily Puzzles:\n  Sharpen your problem-solving skills with a new puzzle every day. Challenge yourself and witness the improvement in your analytical thinking and logical reasoning.\n\n4. Structured Learning Paths:\n   Our courses are meticulously organized into topics and subtopics, providing a clear roadmap for your learning journey. Track your progress easily and stay motivated as you complete each milestone.\n\n5. User-Friendly Interface:\n  Navigate through the app effortlessly with a user-friendly interface. Intuitive design ensures a seamless learning experience, making it easy for users to access courses, blogs, and puzzles.\n\nDesign Team :\n  Varun - 21P31A0541\n   Sai Pradeep - 21P31A0504\n   Ashok Kumar - 21P31A0530\n   Pallavi - 21P31A0521";
+"Welcome to LearnX, your one-stop destination for a diverse range of technology-related courses and insightful blogs. Whether you're a beginner or an experienced professional, our platform is designed to empower you with knowledge across various tech domains. Immerse yourself in a rich learning experience, explore captivating blogs, and challenge your mind with daily puzzles.\n\nKey Features:\n\n1. Extensive Course Library:\n   Dive into our extensive library of technology courses covering a wide array of topics. From programming languages to cutting-edge technologies, we've got you covered.\n\n2. Informative Blogs:\n   Enhance your understanding of technology through freely accessible blogs. Our platform offers a wealth of information, keeping you updated on the latest trends, best practices, and insights from industry experts.\n\n3. Daily Puzzles:\n  Sharpen your problem-solving skills with a new puzzle every day. Challenge yourself and witness the improvement in your analytical thinking and logical reasoning.\n\n4. Structured Learning Paths:\n   Our courses are meticulously organized into topics and subtopics, providing a clear roadmap for your learning journey. Track your progress easily and stay motivated as you complete each milestone.\n\n5. User-Friendly Interface:\n  Navigate through the app effortlessly with a user-friendly interface. Intuitive design ensures a seamless learning experience, making it easy for users to access courses, blogs, and puzzles.\n\nDesign Team :\n  Varun - 21P31A0541\n   Sai Pradeep - 21P31A0504\n   Ashok Kumar - 21P31A0530\n   Pallavi - 21P31A0520";
